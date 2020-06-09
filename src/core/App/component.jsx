ca0cc9007e1d11e53da1df697b356229836b055b
Ryan McHenry <ryanmmchenry@gmail.com>
2020-06-08 20:21:49 -0500

    Wrapping app in ThemeProvider to use theme config

--- src/core/App/component.jsx
+++ src/core/App/component.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useStyles } from "./styles";
 import { Container } from "common";
 import { Sampling } from "pages";
+import { ThemeProvider } from "@material-ui/core";
+import { createTheme } from "core/config/theme";
 
 export const App = () => {
   const classes = useStyles();
   return (
-    <Container maxWidth="md" className={classes.root}>
-      <Sampling />
-    </Container>
+    <ThemeProvider theme={createTheme()}>
+      <Container maxWidth="md" className={classes.root}>
+        <Sampling />
+      </Container>
+    </ThemeProvider>
   );
 };
