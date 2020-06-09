b62970db8519c382b1487ef114c8405db58c6c0c
Ryan McHenry <ryanmmchenry@gmail.com>
2020-06-08 20:14:16 -0500

    Updates

---
+++ precache-manifest.44f20b3cdc1b65223a3ec18aa1e78b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0765a69d91234e2c634566d1edf84288",
+    "url": "/minitab-take-home/index.html"
+  },
+  {
+    "revision": "715943e2b7e8873b04dc",
+    "url": "/minitab-take-home/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd099604bbe1747b6f0f",
+    "url": "/minitab-take-home/static/js/2.c8e48ba8.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/minitab-take-home/static/js/2.c8e48ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715943e2b7e8873b04dc",
+    "url": "/minitab-take-home/static/js/main.e07ed341.chunk.js"
+  },
+  {
+    "revision": "855b18479d14fb794ab4",
+    "url": "/minitab-take-home/static/js/runtime-main.1c6a0079.js"
+  }
+]);
