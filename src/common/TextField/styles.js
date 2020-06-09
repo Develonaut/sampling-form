59e46aa41b60cda2c0a2212cca1de07ef44879bb
Ryan McHenry <ryanmmchenry@gmail.com>
2020-06-08 20:34:57 -0500

    use class specificity to combat jss bug

--- src/common/TextField/styles.js
+++ src/common/TextField/styles.js
@@ -2,6 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    margin: `${theme.spacing(2)}px 0 0 0`,
+    "&.MuiFormControl-root": {
+      marginTop: theme.spacing(2),
+    }
   },
 }));
